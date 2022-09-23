f4b74bc0a77c1503c325d9794ea1ed7d105526d2
taylorsabbag <38229747+taylorsabbag@users.noreply.github.com>
2022-09-23 13:38:43 -0400

    Initial React project set-up + addition of MVP.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,38 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./mvp.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Welcome to React!</h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <header>
+      <nav>
+        <a href="#">
+          <img src="https://via.placeholder.com/200x70?text=WHATTTTT" alt="WHATTTTT" height="70" />
         </a>
-      </header>
-    </div>
+        <ul>
+          <li>Home</li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Store</a>
+            <ul>
+              <li><a href="#">My First Shoes</a></li>
+              <li><a href="#">My Second Shoes</a></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <h1>How are you <i>feeling</i> <u>today?</u></h1>
+      <p>Let's go ahead and get fucked</p>
+      <br />
+      <p>
+        <a href="#">
+          <i>Italic</i>
+        </a>
+        <a href="#">
+          <b>Bold</b>
+        </a>
+      </p>
+    </header>
   );
 }
 
