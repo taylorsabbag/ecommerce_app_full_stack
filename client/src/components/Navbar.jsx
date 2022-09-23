302e7a681f3b52a101130c3938d9d5142efcbfd1
taylorsabbag <38229747+taylorsabbag@users.noreply.github.com>
2022-09-23 14:31:04 -0400

    Added appropriate links and sections to Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,28 +1,42 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
     <header>
       <nav>
         <a href="#">
-          <img src="https://via.placeholder.com/200x70?text=WHATTTTT" alt="WHATTTTT" height="70" />
+          <img
+            src="https://via.placeholder.com/200x70?text=Shoes!"
+            alt="WHATTTTT"
+            height="70"
+          />
         </a>
+        <form action="search">
+            <input type="text" placeholder="Search TODO with button" />
+            <button type="submit">Search</button>
+        </form>
         <ul>
-          <li>Home</li>
           <li>
-            <a href="#">About</a>
+            <a href="#">Account</a>
+            <ul>
+              <li>
+                <a href="#">Register</a>
+              </li>
+              <li>
+                <a href="#">Login</a>
+              </li>
+            </ul>
           </li>
           <li>
-            <a href="#">Store</a>
-            <ul>
-              <li><a href="#">My First Shoes</a></li>
-              <li><a href="#">My Second Shoes</a></li>
-            </ul>
+            <a href="#">Wishlist</a>
+          </li>
+          <li>
+            <a href="#">Cart</a>
           </li>
         </ul>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
